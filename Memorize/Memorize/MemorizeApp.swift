7d47528d8724ea7cf590b022bee8617082c003ea
[treasure3210] <treasure3210@gmail.com>
2023-11-11 13:30:29 -0700

    Initial project setup/commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Derek Kim on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
