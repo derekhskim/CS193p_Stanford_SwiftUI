575da1b7aef26cdf15e1d2b0a107b5018d5a0d71
[treasure3210] <treasure3210@gmail.com>
2023-12-07 23:24:30 -0700

    Create custom CardView for reusability and enhanced readability

---
+++ Memorize/Memorize/View/CardView.swift
@@ -0,0 +1,57 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Derek Kim on 2023-12-07.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+    
+    let card: Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(5)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    typealias Card = CardView.Card
+    
+    static var previews: some View {
+        VStack {
+            HStack {
+                CardView(Card(isFaceUp: true, content: "X", id: "test1"))
+                CardView(Card(content: "X", id: "test1"))
+            }
+            HStack {
+                CardView(Card(isFaceUp: true, isMatched: true, content: "This is a very long string and I hope it fits", id: "test1"))
+                CardView(Card(isMatched: true, content: "X", id: "test1"))
+            }
+        }
+        .padding()
+        .foregroundStyle(.green)
+    }
+}
