6e262f2b8c977c09ead2938ae81baa8f995ea565
[treasure3210] <treasure3210@gmail.com>
2024-01-06 18:59:34 -0700

    Create a general FlyingNumber struct to present an animated score tracker

---
+++ Memorize/Memorize/FlyingNumber.swift
@@ -0,0 +1,22 @@
+//
+//  FlyingNumber.swift
+//  Memorize
+//
+//  Created by Derek Kim on 2024-01-06.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    let number: Int
+    
+    var body: some View {
+        if number != 0 {
+            Text(number, format: .number)
+        }
+    }
+}
+
+#Preview {
+    FlyingNumber(number: 5)
+}
